2e0bb1c67988cc30cddd41ced5582f3128a5d885
VitoriaAlric <vitoriacf@hotmail.com>
2021-01-09 15:30:41 -0300

    Search order location

--- dsdeliver/front-web/src/Orders/Types.ts
+++ dsdeliver/front-web/src/Orders/Types.ts
@@ -1,10 +1,14 @@
 import { type } from "os";
 
 export type Product = {
-    
     id: number;
     name: string;
     price: number;
     description: string;
     imageUri: string;
+}
+export type OrderLocationdata = {
+    address: string;
+    latitude: number;
+    longitude: number;
 }
--- dsdeliver/front-web/src/Orders/index.tsx
+++ dsdeliver/front-web/src/Orders/index.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 import ProductsList from './ProductsList';
 import StepsHeaders from './StepsHeaders';
 import './styles.css';
-import { Product } from './Types';
+import { OrderLocationdata, Product } from './Types';
 
 function Orders () {
   const [products, setProducts] = useState<Product[]>([]);
+  const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
 
   useEffect(() => {
   fetchProducts()
@@ -18,6 +20,7 @@ function Orders () {
     <div className="orders-container">
       <StepsHeaders />
       <ProductsList products={products} />
+      <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
     </div>
     )
 }
